3d90921def5ac7e47efba06ec9148a2429a0081e
webonds2081 <webonds2081@gmail.com>
2021-11-25 01:05:24 +0900

    階層の変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,43 @@
 const { src, dest, watch, series, parallel } = require("gulp");
+
+// 共通
+const rename = require("gulp-rename");
+
+// 読み込み先（階層が間違えていると動かないので注意）
+const srcPath = {
+    css: 'src/sass/**/*.scss',
+    img: 'src/images/**/*',
+    html: './**/*.html'
+}
+
+// 吐き出し先（なければ生成される）
+const destPath = {
+    css: 'css/',
+    img: 'images/'
+}
+
+// ブラウザーシンク（リアルタイムでブラウザに反映させる処理）
+const browserSync = require("browser-sync");
+const browserSyncOption = {
+    server: "./"
+}
+const browserSyncFunc = () => {
+    browserSync.init(browserSyncOption);
+}
+const browserSyncReload = (done) => {
+    browserSync.reload();
+    done();
+}
+
+// Sassファイルのコンパイル処理（DartSass対応）
+const sass = require('gulp-sass')(require('sass'));
+const sassGlob = require('gulp-sass-glob-use-forward');
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
 const postcss = require("gulp-postcss");
 const cssnext = require("postcss-cssnext")
 const cleanCSS = require("gulp-clean-css");
-const rename = require("gulp-rename");
 const sourcemaps = require("gulp-sourcemaps");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
-const imagemin = require("gulp-imagemin");
-const imageminMozjpeg = require("imagemin-mozjpeg");
-const imageminPngquant = require("imagemin-pngquant");
-const imageminSvgo = require("imagemin-svgo");
-const browserSync = require("browser-sync");
-// const fiber = require('fibers');
-//////////////
-// 今までのsass
-//////////////
-const sass = require('gulp-sass')(require('sass'));
-// const sassGlob = require('gulp-sass-glob');
-
-//////////////
-// dartsass
-//////////////
-// const sass = require('gulp-dart-sass');
-const sassGlob = require('gulp-sass-glob-use-forward');
-
-const srcPath = {
-    css: 'src/sass/**/*.scss',
-    js: 'src/js/**/*.js',
-    img: 'src/images/**/*',
-    html: './**/*.html',
-    php: './**/*.php',
-}
-const destPath = {
-    css: 'assets/css/',
-    js: 'assets/js/',
-    img: 'assets/images/'
-}
 const browsers = [
     'last 2 versions',
     '> 5%',
@@ -47,9 +47,7 @@ const browsers = [
     'and_chr >= 5',
     'Android >= 5',
 ]
-const browserSyncOption = {
-  server: "./"
-}
+
 const cssSass = () => {
     return src(srcPath.css)
         .pipe(sourcemaps.init())
@@ -58,80 +56,44 @@ const cssSass = () => {
                 errorHandler: notify.onError('Error:<%= error.message %>')
             }))
         .pipe(sassGlob())
-        // .pipe(sass({ // sass option
-        //     fiber: fiber,
-        //     outputStyle: "expanded"
-        // }))
-        .pipe(sass({
+        .pipe(sass.sync({
             includePaths: ['src/sass'],
             outputStyle: 'expanded'
-        }).on('error', sass.logError)) //指定できるキー expanded compressed
+        }))
         .pipe(postcss([cssnext(browsers)]))
+        .pipe(sourcemaps.write('./'))
         .pipe(dest(destPath.css))
         .pipe(notify({
-            message: 'Sassをコンパイルしました！',
+            message: 'コンパイル出来たよ！',//文字は好きなものに変更してね！
             onLast: true
         }))
 }
 
-const jsBabel = () => {
-    return src(srcPath.js)
-        .pipe(
-            plumber(
-                {
-                    errorHandler: notify.onError('Error: <%= error.message %>')
-                }
-            )
-        )
-        .pipe(babel({
-            presets: ['@babel/preset-env']
-        }))
-        .pipe(dest(destPath.js))
-        .pipe(uglify())
-        .pipe(
-            rename(
-                { extname: '.min.js' }
-            )
-        )
-        .pipe(dest(destPath.js))
-}
+// 画像圧縮
+const imagemin = require("gulp-imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminSvgo = require("imagemin-svgo");
 const imgImagemin = () => {
     return src(srcPath.img)
-        .pipe(
-            imagemin(
-                [
-                    imageminMozjpeg({
-                        quality: 80
-                    }),
-                    imageminPngquant(),
-                    imageminSvgo({
-                        plugins: [
-                            {
-                                removeViewbox: false
-                            }
-                        ]
-                    })
-                ],
-                {
-                    verbose: true
-                }
-            )
-        )
-        .pipe(dest(destPath.img))
+    .pipe(imagemin([
+        imageminMozjpeg({quality: 80}),
+        imageminPngquant(),
+        imageminSvgo({plugins: [{removeViewbox: false}]})
+        ],
+        {
+            verbose: true
+        }
+    ))
+    .pipe(dest(destPath.img))
 }
-const browserSyncFunc = () => {
-    browserSync.init(browserSyncOption);
-}
-const browserSyncReload = (done) => {
-    browserSync.reload();
-    done();
-}
+
+// ファイルの変更を検知
 const watchFiles = () => {
     watch(srcPath.css, series(cssSass, browserSyncReload))
-    watch(srcPath.js, series(jsBabel, browserSyncReload))
     watch(srcPath.img, series(imgImagemin, browserSyncReload))
     watch(srcPath.html, series(browserSyncReload))
-    watch(srcPath.php, series(browserSyncReload))
 }
-exports.default = series(series(cssSass, jsBabel, imgImagemin), parallel(watchFiles, browserSyncFunc));
-exports.build = series(cssSass, jsBabel, imgImagemin);
+
+// npx gulpで出力する内容
+exports.default = series(series(cssSass, imgImagemin), parallel(watchFiles, browserSyncFunc));
